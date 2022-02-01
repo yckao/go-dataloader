99ad095a298c54cf52b2c45223c5d5bd7de12b0b
yckao <yckaokarl@gmail.com>
2022-02-01 16:48:04 +0800

    docs: add basic usage to readme and examples

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,22 +1,3 @@
-# go-dataloader
-
-A clean, safe, user-friendly implementation of [GraphQL's Dataloader](https://github.com/graphql/dataloader), written with generics in go (go1.18beta1).
-
-## Features
-
-- written in generics with strong type
-- nearly same interface with original nodejs version dataloader
-- promise like thunk design, simply call `val, err := loader.Load(ctx, id).Get(ctx)`
-- customizable cache, easily wrap lru or redis.
-- customizable scheduler, can manual dispatch, or use time window (default)
-
-## Requirement
-
-- Only support go >= 1.18, currently in beta
-
-## Getting Started
-
-```go
 package main
 
 import (
@@ -51,11 +32,3 @@ func main() {
     val, err := thunk.Get(ctx)
     fmt.Printf("value: %v, err: %v\n", val, err)
 }
-```
-
-## TODO
-
-- [ ] Examples
-- [ ] Docs
-- [ ] Support hooks for observability
-- [ ] Rewrite tests with mock (waiting for mockgen support 1.18)
