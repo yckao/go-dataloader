05ff18295335d8f66dc6d00ffc5f54d33cec68fd
YC Kao <yckaokarl@gmail.com>
2022-12-29 12:41:21 +0800

    chore: optimize thunk

--- thunk.go
+++ thunk.go
@@ -6,55 +6,53 @@ import (
 
 type Thunk[V interface{}] struct {
 	pending chan bool
-	value   chan V
-	err     chan error
+	data    chan *thunkData[V]
+}
+
+type thunkData[V interface{}] struct {
+	value V
+	err   error
 }
 
 func NewThunk[V interface{}]() *Thunk[V] {
-	thunk := &Thunk[V]{
+	Thunk := &Thunk[V]{
 		pending: make(chan bool, 1),
-		value:   make(chan V, 1),
-		err:     make(chan error, 1),
+		data:    make(chan *thunkData[V], 1),
 	}
 
-	thunk.pending <- true
+	Thunk.pending <- true
 
-	return thunk
+	return Thunk
 }
 
 func (a *Thunk[V]) Get(ctx context.Context) (V, error) {
 	select {
 	case <-ctx.Done():
 		return *new(V), ctx.Err()
-	case v := <-a.value:
-		a.value <- v
-		return v, nil
-	case err := <-a.err:
-		a.err <- err
-		return *new(V), err
+	case v := <-a.data:
+		a.data <- v
+		return v.value, v.err
 	}
 }
 
 func (a *Thunk[V]) set(ctx context.Context, value V) (V, error) {
 	select {
-	case <-a.err:
-	case <-a.value:
+	case <-a.data:
 	case <-a.pending:
 	}
 
-	a.value <- value
+	a.data <- &thunkData[V]{value: value}
 
 	return a.Get(ctx)
 }
 
 func (a *Thunk[V]) error(ctx context.Context, err error) (V, error) {
 	select {
-	case <-a.err:
-	case <-a.value:
+	case <-a.data:
 	case <-a.pending:
 	}
 
-	a.err <- err
+	a.data <- &thunkData[V]{err: err}
 
 	return a.Get(ctx)
 }
